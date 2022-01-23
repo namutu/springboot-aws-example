6478e315bc068d62a5602f96a3b732e13c891f68
namutu <mikoow3@naver.com>
2022-01-24 00:51:25 +0900

    2Day

--- src/main/java/com/mik/spring/service/posts/PostsService.java
+++ src/main/java/com/mik/spring/service/posts/PostsService.java
@@ -1,5 +1,21 @@
 package com.mik.spring.service.posts;
 
+import com.mik.spring.domain.posts.PostsRepository;
+import com.mik.spring.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 
 }
--- src/main/java/com/mik/spring/web/PostsApiController.java
+++ src/main/java/com/mik/spring/web/PostsApiController.java
@@ -1,4 +1,21 @@
 package com.mik.spring.web;
 
+import com.mik.spring.service.posts.PostsService;
+import com.mik.spring.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
 }
